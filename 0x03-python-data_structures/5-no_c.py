23f0a438ca7d91788cea36f4d2fccc22f8397cca
Mausiz <mosesmefray@gmail.com>
2023-09-11 20:01:01 -0700

    ********

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 5-no_c.py
+
+def no_c(my_string):
+    """ removes all characters c and C from a string."""
+    copy = [x for x in my string if x != 'c' and x != 'C']
+    return ("".join(copy))
